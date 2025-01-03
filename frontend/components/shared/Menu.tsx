735674b00f3c541d55c683ad3b1d45a1fbd9a8da
Claude Joseph-Angélique <claude.josephangelique@gmail.com>
2025-01-03 23:46:58 +0100

    The Admin menu is displayed only if the connected wallet is the Fondation owner

--- frontend/components/shared/Menu.tsx
+++ frontend/components/shared/Menu.tsx
@@ -1,13 +1,24 @@
 'use client'
 
 import Link from "next/link";
+import { useAccount, useReadContract } from "wagmi";
+import { fondation } from "@/utils/contract";
 
 const Menu = () => {
+
+    const { isConnected, address } = useAccount()
+
+    const { data: owner } = useReadContract({
+        abi: fondation.abi,
+        address: fondation.address,
+        functionName: "owner"
+    })
+
     return (
         <ul className="menu">
             <li><Link href="/">Home</Link></li>
             <li><Link href="/stats">Stats</Link></li>
-            <li><Link href="/admin">Admin</Link></li>
+            { isConnected && owner === address ? <li><Link href="/admin">Admin</Link></li> : "" }
         </ul>
     )
 }
